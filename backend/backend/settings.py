f8e24075cdbfad60ddda7051f9ce78ec9950eea1
k2foxspb <k2foxpsb@mail.ru>
2023-10-25 17:01:38 +0300

    main

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -35,7 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    'main_app.apps.MainAppConfig'
 ]
 
 MIDDLEWARE = [
---
+++ backend/main_app/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from main_app.apps import MainAppConfig
+
+app_name = MainAppConfig.name
+
+urlpatterns = [
+
+]
