6191ba44e01dd3ae0faca7c77825553b21655924
k2foxspb <k2foxspb@mail.ru>
2023-11-17 17:22:53 +0300

    admin.py

--- backend/main_app/admin.py
+++ backend/main_app/admin.py
@@ -2,6 +2,23 @@ from django.contrib import admin
 from main_app import models
 
 
+@admin.action(description='пометить статью удалённой')
+def make_deleted(modeladmin, request, queryset):
+    queryset.update(status='del')
+
+
+@admin.action(description='опубликовать статью')
+def make_published(modeladmin, request, queryset):
+    queryset.update(status='pu')
+
+
 @admin.register(models.Article)
 class ArticleAdmin(admin.ModelAdmin):
+    list_display = ['title', 'created', 'status']
     search_fields = ['title']
+    actions = [make_published, make_deleted]
+
+
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['title']
--- backend/main_app/models.py
+++ backend/main_app/models.py
@@ -15,28 +15,36 @@ def news_image_path(instance, filename):
     return f'news_{instance.title}/image/pic_{num}{suf}'
 
 
+STATUS_CHOICES = [
+    ('del', 'Delete'),
+    ('pu', 'Published'),
+    ('wi', 'Withdrawn')
+]
+
+
 class Article(models.Model):
     title = models.CharField(max_length=256, unique=True, verbose_name="Заголовок")
     preamble = models.CharField(max_length=1024, verbose_name="Преамбула")
+    category = models.ForeignKey('Category', on_delete=models.CASCADE, verbose_name='Категория')
     body = models.TextField(blank=True, null=True, verbose_name="Текст")
     created = models.DateTimeField(auto_now_add=True, verbose_name="Создано", editable=False)
     updated = models.DateTimeField(auto_now=True, verbose_name="Отредактировано", editable=False)
-    deleted = models.BooleanField(default=False, verbose_name='Пометить как удалённую')
     slug = models.CharField(verbose_name='URL-адрес', max_length=255, blank=True, unique=True, editable=False)
     image = models.ImageField(upload_to=news_image_path, null=True, blank=True, verbose_name='Иконка')
     image1 = models.ImageField(upload_to=news_image_path, blank=True)
     image2 = models.ImageField(upload_to=news_image_path, blank=True)
     image3 = models.ImageField(upload_to=news_image_path, blank=True)
     keyword = models.CharField(max_length=255, blank=True, verbose_name='ключевые слова')
+    status = models.CharField(max_length=3, choices=STATUS_CHOICES, default='pu')
 
     def get_absolute_url(self):
         return reverse("main:news_detail", kwargs={"slug": self.slug})
 
     def __str__(self) -> str:
-        return f"{self.pk} {self.title}"
+        return f"{self.title}"
 
     def delete(self, *args):
-        self.deleted = True
+        self.status = 'del'
         self.save()
 
     def save(self, *args, **kwargs):
@@ -51,3 +59,22 @@ class Article(models.Model):
         verbose_name = "Статья"
         verbose_name_plural = "Статьи"
         ordering = ("-created",)
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=255, unique=True, verbose_name='Заголовок')
+    slug = models.CharField(verbose_name='URL-адрес', max_length=255, blank=True, unique=True, editable=False)
+
+    def get_absolute_url(self):
+        return reverse("", kwargs={"slug": self.slug})
+
+    def save(self, *args, **kwargs):
+        """
+        Сохранение полей модели при их отсутствии заполнения
+        """
+        if not self.slug:
+            self.slug = unique_slugify(self, self.title)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
