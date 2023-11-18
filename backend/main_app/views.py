aa982bd4d8f69568032f305df931924a5a0b791b
k2foxspb <k2foxspb@mail.ru>
2023-11-18 22:38:17 +0300

    style

--- backend/main_app/views.py
+++ backend/main_app/views.py
@@ -28,7 +28,7 @@ class ArticleDetailView(DetailView):
 
 class ArticleUpdateView(PermissionRequiredMixin, UpdateView):
     model = models.Article
-    fields = ('preamble', 'body', 'deleted', 'keyword', 'image')
+    fields = ('title', 'preamble', 'body', 'keyword', 'category', 'image')
     success_url = reverse_lazy("main:main")
     permission_required = ("main_app.change_article",)
     template_name = 'main/articles/articles_form.html'
