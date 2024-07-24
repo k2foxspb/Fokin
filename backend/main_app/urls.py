08e9a2fe0f2a318b304fae110f4af67efcbf90c5
k2fox <k2foxspb@mail.ru>
2024-07-24 15:02:00 +0300

    пока так

--- backend/main_app/urls.py
+++ backend/main_app/urls.py
@@ -16,22 +16,22 @@ app_name = MainAppConfig.name
 urlpatterns = [
     path('', Main.as_view(), name='main'),
     path('about/', About_me.as_view(), name='about'),
-    # path('category/<slug:slug>/', CategoryDetailView.as_view(), name='category_detail'),
-    # path('home/', CategoryListView.as_view(), name='main_category'),
-    # path("article/create/", ArticleCreateView.as_view(), name="article_create"),
-    # path(
-    #     "article/<slug:slug>/",
-    #     ArticleDetailView.as_view(),
-    #     name="article_detail",
-    # ),
-    # path(
-    #     "article/<slug:slug>/update",
-    #     ArticleUpdateView.as_view(),
-    #     name="article_update",
-    # ),
-    # path(
-    #     "article/<slug:slug>/delete",
-    #     ArticleDeleteView.as_view(),
-    #     name="article_delete",
-    # ),
+    path('category/<slug:slug>/', CategoryDetailView.as_view(), name='category_detail'),
+    path('home/', CategoryListView.as_view(), name='main_category'),
+    path("article/create/", ArticleCreateView.as_view(), name="article_create"),
+    path(
+        "article/<slug:slug>/",
+        ArticleDetailView.as_view(),
+        name="article_detail",
+    ),
+    path(
+        "article/<slug:slug>/update",
+        ArticleUpdateView.as_view(),
+        name="article_update",
+    ),
+    path(
+        "article/<slug:slug>/delete",
+        ArticleDeleteView.as_view(),
+        name="article_delete",
+    ),
 ]
