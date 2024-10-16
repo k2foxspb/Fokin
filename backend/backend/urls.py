0b29c3ddfadd9f097c383187359c074e0ebc5871
Valeriy Fokin <k2foxspb@mail.ru>
2024-10-16 11:40:29 +0300

    django-filer 3.2.3

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -21,6 +21,7 @@ from backend import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('filer/', include('filer.urls')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
     path('admin/', admin.site.urls),
     path('', include('main_app.urls', namespace='main')),
