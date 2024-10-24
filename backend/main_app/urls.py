cf8a23a443bcb34ad35aa19784cb7d68fc9cb39c
Valeriy Fokin <k2foxspb@mail.ru>
2024-10-24 11:08:01 +0300

    удалил ненужное

--- backend/main_app/urls.py
+++ backend/main_app/urls.py
@@ -1,10 +1,7 @@
 from django.urls import path
 
 from main_app.views import (
-    ArticleCreateView,
     ArticleDetailView,
-    ArticleUpdateView,
-    ArticleDeleteView,
     CategoryListView,
     CategoryDetailView,
     About_me
@@ -17,20 +14,9 @@ urlpatterns = [
     path('about/', About_me.as_view(), name='about'),
     path('category/<slug:slug>/', CategoryDetailView.as_view(), name='category_detail'),
     path('', CategoryListView.as_view(), name='main_category'),
-    path("article/create/", ArticleCreateView.as_view(), name="article_create"),
     path(
         "article/<slug:slug>/",
         ArticleDetailView.as_view(),
         name="article_detail",
-    ),
-    path(
-        "article/<slug:slug>/update",
-        ArticleUpdateView.as_view(),
-        name="article_update",
-    ),
-    path(
-        "article/<slug:slug>/delete",
-        ArticleDeleteView.as_view(),
-        name="article_delete",
     ),
 ]
--- backend/main_app/views.py
+++ backend/main_app/views.py
@@ -36,29 +36,6 @@ class CategoryDetailView(DetailView):
     template_name = "main/articles/category_detail.html"
 
 
-class ArticleCreateView(PermissionRequiredMixin, CreateView):
-    model = models.Article
-    fields = '__all__'
-    success_url = reverse_lazy("main:main")
-    permission_required = ("main_app.add_article",)
-    template_name = "main/articles/articles_form.html"
-
-
 class ArticleDetailView(DetailView):
     model = models.Article
     template_name = "main/articles/articles_detail.html"
-
-
-class ArticleUpdateView(PermissionRequiredMixin, UpdateView):
-    model = models.Article
-    fields = '__all__'
-    success_url = reverse_lazy("main:main_category")
-    permission_required = ("main_app.change_article",)
-    template_name = "main/articles/articles_form.html"
-
-
-class ArticleDeleteView(PermissionRequiredMixin, DeleteView):
-    model = models.Article
-    success_url = reverse_lazy("main:main_category")
-    permission_required = ("main_app.delete_article",)
-    template_name = "main/articles/articles_confirm_delete.html"
