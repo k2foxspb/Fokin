56d0903d0dd29b9b44b541a35cb3ba6271e15962
Valeriy Fokin <k2foxspb@mail.ru>
2024-10-31 14:49:07 +0300

    chat chanel

--- backend/chatapp/routing.py
+++ backend/chatapp/routing.py
@@ -3,5 +3,5 @@ from django.urls import path, re_path
 from chatapp import consumers
 
 websocket_urlpatterns = [
-    re_path(r'ws/chat/(?P<room_name>\w+)/$', consumers.ChatConsumer.as_asgi()),
+    re_path(r'wss/chat/(?P<room_name>\w+)/$', consumers.ChatConsumer.as_asgi()),
 ]
