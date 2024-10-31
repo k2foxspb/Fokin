078a5640b5a8b0b8a8c398adcf5721886139c8b8
Valeriy Fokin <k2foxspb@mail.ru>
2024-10-31 08:13:04 +0300

    chat chanel

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -13,14 +13,17 @@ from channels.auth import AuthMiddlewareStack
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
-import chatapp.routing
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", 'backend.settings')
+django_asgi_app = get_asgi_application()
+
+from chatapp import routing
+
 
 application = ProtocolTypeRouter({
-  'http': get_asgi_application(),
+  'http': django_asgi_app,
   'websocket': AllowedHostsOriginValidator(AuthMiddlewareStack(
         URLRouter(
-            chatapp.routing.websocket_urlpatterns
+            routing.websocket_urlpatterns
         )
     ),)
 })
