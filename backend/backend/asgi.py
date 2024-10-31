a94cb3ddf8bb3a6894f3056555ac36cb391715c9
Valeriy Fokin <k2foxspb@mail.ru>
2024-10-31 07:30:48 +0300

    chat chanel

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -18,9 +18,9 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
 application = ProtocolTypeRouter({
   'http': get_asgi_application(),
-  'websocket': AuthMiddlewareStack(
+  'websocket': AllowedHostsOriginValidator(AuthMiddlewareStack(
         URLRouter(
             chatapp.routing.websocket_urlpatterns
         )
-    ),
+    ),)
 })
