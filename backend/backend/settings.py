f9621aa3b6446063e9a31b092993649d511e32e3
k2fox <k2foxspb@mail.ru>
2025-03-26 10:02:41 +0300

    migrations

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -282,6 +282,6 @@ if os.environ.get("AWS_ACCESS_KEY_ID"):
 AWS_DEFAULT_REGION = 'ru-central1'
 AWS_STORAGE_BUCKET_NAME = 'fokin.fun'
 AWS_S3_ENDPOINT_URL = 'https://storage.yandexcloud.net/'
-# AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
-# AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 # storage and image = models.FileField(upload_to='my_files/', storage=default_storage)
