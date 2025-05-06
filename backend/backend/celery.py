a44585a01cac09e9cf2dcb88e89c834a92e6ded8
k2fox <k2foxspb@mail.ru>
2025-05-06 16:55:43 +0300

    fix

--- backend/backend/celery.py
+++ backend/backend/celery.py
@@ -6,3 +6,4 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.backend.settings')
 app = Celery('Fokin')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
+app.conf.broker_url = 'redis://127.0.0.1:6379'
