60082a37ca1d1aaf9bcb4b44d4b349b1f4e82a63
k2fox <k2foxspb@mail.ru>
2025-05-06 22:04:47 +0300

    storage yandex

--- backend/backend/celery.py
+++ backend/backend/celery.py
@@ -6,4 +6,3 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 app = Celery('backend', broker=settings.BROKER_URL)
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
-app.conf.broker_url = 'redis://127.0.0.1:6379'
