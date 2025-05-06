33a258ba67a44edfa33a207dfd85eebe859dc7d0
k2fox <k2foxspb@mail.ru>
2025-05-06 22:27:12 +0300

    storage yandex

--- backend/backend/celery.py
+++ backend/backend/celery.py
@@ -1,8 +1,7 @@
 import os
-
 from celery import Celery
-from django.conf import settings
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
-app = Celery('backend', broker=settings.BROKER_URL)
-app.config_from_object('django.conf:settings', namespace='CELERY')
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.backend.settings")
+app = Celery("backend.backend")
+app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
