20a84df3c2559745b001eb231c20b2d8f1aa47d6
k2fox <k2foxspb@mail.ru>
2025-05-06 16:13:26 +0300

    fix

---
+++ backend/backend/celery.py
@@ -0,0 +1,8 @@
+import os
+
+from celery import Celery
+from django.conf import settings
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Fokin.backend.backend.settings')
+app = Celery('Fokin')
+app.config_from_object('django.conf:settings', namespace='CELERY')
+app.autodiscover_tasks()
