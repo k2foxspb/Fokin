d5b28747c07bb1f5a5b288b65eed021cb2e01b7a
k2fox <k2foxspb@mail.ru>
2025-05-06 21:11:26 +0300

    storage yandex

--- backend/backend/celery.py
+++ backend/backend/celery.py
@@ -2,8 +2,8 @@ import os
 
 from celery import Celery
 from django.conf import settings
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.backend.settings')
-app = Celery('Fokin', broker=settings.BROKER_URL)
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+app = Celery('backend', broker=settings.BROKER_URL)
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
 app.conf.broker_url = 'redis://127.0.0.1:6379'
