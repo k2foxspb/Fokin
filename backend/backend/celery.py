e08094db3689e8ada0cb493387813e18fd6bb6ba
k2fox <k2foxspb@mail.ru>
2025-05-07 10:57:25 +0300

    fix

--- backend/backend/celery.py
+++ backend/backend/celery.py
@@ -1,12 +1,9 @@
 import os
 
 from celery import Celery
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+app = Celery('celery', broker=' redis://localhost')
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.backend.settings')
-app = Celery('celery')
-app.config_from_object('django.conf:settings', namespace='CELERY')
-app.conf.broker_url = 'redis://127.0.0.1:6379'
-app.autodiscover_tasks()
-
-
-
+@app.task
+def add(x, y):
+    return x + y
