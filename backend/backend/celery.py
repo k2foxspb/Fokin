d48ec1487227a4c41e112bd202ec50b565b6dc88
k2fox <k2foxspb@mail.ru>
2025-05-07 13:17:40 +0300

    fix

--- backend/backend/celery.py
+++ backend/backend/celery.py
@@ -3,7 +3,7 @@ from django.conf import settings
 from celery import Celery
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
-app = Celery('celery')
+app = Celery('backend')
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
 @app.task
