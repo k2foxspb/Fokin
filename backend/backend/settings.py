e6f280e0fa568deda8e21fa7943c883c66f02734
k2fox <k2foxspb@mail.ru>
2025-06-06 07:55:40 +0300

    fix

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -131,7 +131,8 @@ AUTHENTICATION_BACKENDS = ["authapp.backend.UserModelBackend"]
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.mail.ru"
 EMAIL_HOST_USER = "k2foxspb@mail.ru"
-EMAIL_HOST_PASSWORD = env('EMAIL_HOST')
+EMAIL_HOST_PASSWORD = 'I0qt5QKAhwGhe8BsqpOn'
+print(EMAIL_HOST_PASSWORD)
 EMAIL_PORT = 465
 EMAIL_USE_SSL = True
 DEFAULT_FROM_EMAIL = 'k2foxspb@mail.ru'
