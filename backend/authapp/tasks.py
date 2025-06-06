68cab3d9ca6a398a2206b011d74f9e28d490b49f
k2fox <k2foxspb@mail.ru>
2025-06-06 11:23:04 +0300

    fix

--- backend/authapp/tasks.py
+++ backend/authapp/tasks.py
@@ -2,10 +2,11 @@ from time import sleep
 from django.core.mail import send_mail
 from celery import shared_task
 from authapp.models import CustomUser
+from backend.celery import app
 from backend.settings import EMAIL_HOST_USER
 
 
-@shared_task()
+@app.task
 def send_feedback_email_task(email, firs_name, last_name):
     """Sends an email when the feedback form has been submitted."""
     send_mail(
@@ -17,7 +18,7 @@ def send_feedback_email_task(email, firs_name, last_name):
     )
 
 
-@shared_task()
+@app.task
 def send_feedback_email_task_update(email, firs_name, last_name):
     """Sends an email when the feedback form has been submitted."""
     send_mail(
@@ -29,7 +30,7 @@ def send_feedback_email_task_update(email, firs_name, last_name):
     )
 
 
-@shared_task()
+@app.task
 def delete_unconfirmed_user(user_id):
    sleep(900)
    user = CustomUser.objects.get(pk=user_id)
