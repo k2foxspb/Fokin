180b23a4e9fa5fc8fcca5a067d07ac27315d8269
k2fox <k2foxspb@mail.ru>
2025-07-26 09:16:27 +0300

    удаление пользователя через минуту

--- my-mobile-app/app/config.tsx
+++ my-mobile-app/app/config.tsx
@@ -3,8 +3,8 @@ const DEV = process.env.NODE_ENV === 'development';
 
 export const API_CONFIG = {
   // Базовый URL для HTTP запросов
-  BASE_URL: DEV ? 'http://localhost:8000' : 'https://fokin.fun',
+  BASE_URL: DEV ? 'http://localhost:8000' : 'http://localhost:8000',
 
   // Базовый URL для WebSocket соединений
-  WS_URL: DEV ? 'ws://127.0.0.1:8000' : 'wss://fokin.fun',
+  WS_URL: DEV ? 'ws://127.0.0.1:8000' : 'ws://127.0.0.1:8000',
 };
