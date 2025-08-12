d591b093f69fc688008c8714916dc20658d40016
k2fox <k2foxspb@mail.ru>
2025-08-12 13:27:44 +0300

    front

--- backend/chatapp/routing.py
+++ backend/chatapp/routing.py
@@ -6,6 +6,6 @@ websocket_urlpatterns = [
     re_path(r"wss/notification/", NotificationConsumer.as_asgi()),
     re_path(r'wss/chat/(?P<room_name>[^/]+)', ChatConsumer.as_asgi()),
     re_path(r'wss/private/(?P<room_name>[^/]+)',PrivateChatConsumer.as_asgi()),
-    re_path(r'wss/chat_list/', ChatListConsumer.as_asgi()),
+    re_path(r'^wss/chat_list/', ChatListConsumer.as_asgi()),
 
 ]
