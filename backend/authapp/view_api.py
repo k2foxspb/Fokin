9eb1e5d4704551a1cd9508c24b7ecbb20217695a
k2fox <k2foxspb@mail.ru>
2025-08-12 09:23:39 +0300

    front

--- backend/authapp/view_api.py
+++ backend/authapp/view_api.py
@@ -1,6 +1,6 @@
 from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
-
+from rest_framework.permissions import IsAuthenticated
 
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
